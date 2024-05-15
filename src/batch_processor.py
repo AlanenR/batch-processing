65448686bc72a5300adcba2d1e16b2d6191f9f47
Riikka Alanen <riikka.alanen@kuubi.fi>
2024-05-15 11:24:09 +0300

    Separate functions

--- src/batch_processor.py
+++ src/batch_processor.py
@@ -3,35 +3,36 @@ from config import MAX_RECORD_SIZE, MAX_BATCH_SIZE, MAX_NUMBER_OF_RECORDS
 def get_size(record):
     return len(record.encode('utf-8'))
 
+def validate_record(record, max_record_size):
+    if type(record) != str:
+        return False
+    if get_size(record) > max_record_size:
+        return False
+    return True
+
+
+def add_record_to_batch(record, batch, batches, current_batch_size, max_batch_size, max_number_of_records):
+    record_size = get_size(record)
+    if (current_batch_size + record_size > max_batch_size) or (len(batch) >= max_number_of_records):
+        batches.append(batch)
+        batch = []
+        current_batch_size = 0
+    batch.append(record)
+    current_batch_size += record_size
+    return batch, batches, current_batch_size
+
 
 def process_batch(records, 
                   max_record_size=MAX_RECORD_SIZE, 
                   max_batch_size=MAX_BATCH_SIZE, 
-                  max_number_of_records=MAX_NUMBER_OF_RECORDS
-                  ):
+                  max_number_of_records=MAX_NUMBER_OF_RECORDS):
+    batch = [] 
     batches = []
-    batch = []
     current_batch_size = 0
-
-    for record in records:
-        if type(record) != str:
-            continue
-
-        record_size = get_size(record)
-
-        if record_size > max_record_size:
-            continue
-
-        if (current_batch_size + record_size > max_batch_size 
-            or len(batch) == max_number_of_records):
-            batches.append(batch)
-            batch = []
-            current_batch_size = 0
-
-        current_batch_size += record_size
-        batch.append(record)
-
+    for record in records: 
+        if validate_record(record, max_record_size):
+            batch, batches, current_batch_size = add_record_to_batch(
+                record, batch, batches, current_batch_size, max_batch_size, max_number_of_records)       
     if batch:
         batches.append(batch)
-    
     return batches
